b48d68083d0ea403628d7eaee76e49439564bad3
naftalimurgor <murgornaftali@gmail.com>
2022-09-22 21:52:10 +0300

    fix typo install bash script

--- install-composer-linux.sh
+++ install-composer-linux.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-# info, install Composer programmatically: https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
-
-EXPECTED_CHECKSUM="$(php -r 'copy -r ("https://composer.github.io/installer.sig", "php://stdout");')"
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-if ["$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM"]
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
 then
     >&2 echo 'ERROR: Invalid installer checksum'
     rm composer-setup.php
@@ -15,5 +13,5 @@ fi
 
 php composer-setup.php --quiet
 RESULT=$?
-
+rm composer-setup.php
 exit $RESULT
